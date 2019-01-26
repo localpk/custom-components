88f3d1cb8042d4ea5cdcbc08a3584d3495c1e548
Jabran Rafique <hello@jabran.me>
2019-01-26 03:22:09 +0000

    add component

---
+++ src/HighlightedNumbers/index.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from 'react';
+
+export default function HighlightedNumbers({ data }) {
+  const dataAsArray = JSON.parse(data);
+
+  return (
+    <div className='row text-center pt-2 pb-5'>
+      {dataAsArray.map((item) => {
+        return (
+          <Fragment key={item.description}>
+            <div className='col-sm-6 col-md-3 py-3'>
+              <div className='display-3'>
+                <strong>{item.numbers}</strong>
+              </div>
+              <div className='text-muted'>{item.description}</div>
+            </div>
+          </Fragment>
+        );
+      })}
+    </div>
+  );
+}
